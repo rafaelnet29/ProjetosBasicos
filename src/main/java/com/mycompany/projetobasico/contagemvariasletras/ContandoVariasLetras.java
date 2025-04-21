83b1b196b60b93705a5def11f375e29c1bb4b391
rafaelnet29 <rafaelsouzanet29@outlook.com>
2025-04-21 14:08:32 -0300

    -Identação

--- src/main/java/com/mycompany/projetobasico/contagemvariasletras/ContandoVariasLetras.java
+++ src/main/java/com/mycompany/projetobasico/contagemvariasletras/ContandoVariasLetras.java
@@ -9,26 +9,27 @@ import java.util.Scanner;
  * @author rafae
  */
 public class ContandoVariasLetras {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         HashMap<Character, Integer> mapaLetras = new HashMap<>();
-        String frase= "";
-        
+        String frase = "";
+
         System.out.println("Informe uma frase:");
         frase = scan.nextLine().replace(" ", "");
-        
+
         for (int i = 0; i < frase.length(); i++) {
 
             char c = frase.charAt(i);
             if (Character.isLetter(c) && mapaLetras.containsKey(c)) {
                 mapaLetras.put(c, mapaLetras.get(c) + 1);
-            }else{
+            } else {
                 mapaLetras.put(c, 1);
             }
         }
         for (Map.Entry<Character, Integer> entry : mapaLetras.entrySet()) {
-            System.out.println("A letra: '" + entry.getKey()+ "' aparece " + entry.getValue() + " vezes ");
+            System.out.println("A letra: '" + entry.getKey() + "' aparece " + entry.getValue() + " vezes ");
         }
-        
+
     }
 }
