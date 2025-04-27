9322409ea6e3cf9215c960a901e79efb7a799392
rafaelnet29 <rafaelsouzanet29@outlook.com>
2025-04-27 12:04:08 -0300

    - Adicionado mais um projeto - Numeros Repetidos - Fequencia de numeros apenas comments

--- src/main/java/com/mycompany/projetobasico/númerosrepetidos/NumerosRepetidos.java
+++ src/main/java/com/mycompany/projetobasico/númerosrepetidos/NumerosRepetidos.java
@@ -1,4 +1,4 @@
-package com.mycompany.projetobasico.frequenciadenumeros;
+package com.mycompany.projetobasico.númerosrepetidos;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,37 +6,32 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class FrequenciaNumeros {
+public class NumerosRepetidos {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         HashMap<Integer, Integer> mapaNumeros = new HashMap<Integer, Integer>();
         List<Integer> listaNumeros = new ArrayList<Integer>();
-        int contador = 0;
-        int temp = 0;
 
-        System.out.println("Digite uma lista de numeros e ZERO para sair:");
+        int contador = 0;
+
+        System.out.println("Digite uma lista de numero: informe ZERO para finalizar");
         do {
             contador = scan.nextInt();
             listaNumeros.add(contador);
         } while (contador != 0);
 
-        listaNumeros.remove(listaNumeros.size() - 1);//remove o zero
+        listaNumeros.remove(listaNumeros.size() - 1);
 
         for (int num : listaNumeros) {
-            //+1 conta a frequencia de aparições do numero
             mapaNumeros.put(num, mapaNumeros.getOrDefault(num, 0) + 1);
-        }
-        for(int num : listaNumeros){
-            if (mapaNumeros.get(num) == 1) {
-                    temp += num;
-                }
         }
 
         for (Map.Entry<Integer, Integer> entry : mapaNumeros.entrySet()) {
             Integer key = entry.getKey();
             Integer value = entry.getValue();
-            System.out.println("Numeros frequentes: " + key + " >>> " + value);
+            System.out.println("Lista atualizada: " + key + " -> aparece -> " + value + " vezes ");
         }
+
     }
 }
